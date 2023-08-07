63b390ec609d72d60b98a8fdd183b5af492b3d3a
LeeJS9856 <wnstj1057@naver.com>
2023-08-08 01:11:03 +0900

    FIX : Modify edit_text delete funtion

--- app/src/main/java/org/techtown/mealproject/AddMenuFragment.java
+++ app/src/main/java/org/techtown/mealproject/AddMenuFragment.java
@@ -12,6 +12,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -54,37 +55,64 @@ public class AddMenuFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.add_menu_fragment, container, false);
-
-
         initUI(rootView);
-
         return rootView;
     }
 
 
     private void initUI(ViewGroup rootView) {
         initRecyView(rootView);
+        initAddMenu(rootView);
 
+
+    }
+
+    public void initRecyView(ViewGroup rootView) {
+        addMenuRecyclerView = rootView.findViewById(R.id.add_menu_recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        menuItemAdapter = new MenuItemAdapter(context);
+        addMenuRecyclerView.setLayoutManager(layoutManager);
+        addMenuRecyclerView.setAdapter(menuItemAdapter);
+    }
+
+    void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    public void initAddMenu(ViewGroup rootView) {
         Button addMenuButton = rootView.findViewById(R.id.add_menu_button);
         EditText editText = rootView.findViewById(R.id.editText);
         addMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                menuItemAdapter.addItem(editText.getText().toString());
-                addMenuRecyclerView.setAdapter(menuItemAdapter);
-                editText.setText(null);
-                hideKeyboard();
+                if(editText.length()!=0) {
+                    menuItemAdapter.addItem(editText.getText().toString());
+                    addMenuRecyclerView.setAdapter(menuItemAdapter);
+                    editText.setText(null);
+                    hideKeyboard();
+                }
+                else {
+                    Toast.makeText(getContext(), "메뉴를 입력하세요", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                switch (keyCode) {
-                    case KeyEvent.KEYCODE_ENTER:
-                        addMenuButton.callOnClick();
-                        break;
+                if(keyCode == KeyEvent.KEYCODE_ENTER) {
+                    addMenuButton.callOnClick();
+                } else if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_DEL)) {
+                    String str = editText.getText().toString();
+                    if (editText.length() > 0) {
+                        str = str.substring(0, str.length() - 1);
+                        editText.setText(str);
+                        editText.setSelection(editText.length());
+                    }
                 }
+
                 return true;
             }
         });
@@ -100,20 +128,6 @@ public class AddMenuFragment extends Fragment {
                 return handled;
             }
         });
-
-    }
-
-    public void initRecyView(ViewGroup rootView) {
-        addMenuRecyclerView = rootView.findViewById(R.id.add_menu_recyclerView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
-        menuItemAdapter = new MenuItemAdapter(context);
-        addMenuRecyclerView.setLayoutManager(layoutManager);
-        addMenuRecyclerView.setAdapter(menuItemAdapter);
-    }
-
-    void hideKeyboard() {
-        InputMethodManager inputManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
 
